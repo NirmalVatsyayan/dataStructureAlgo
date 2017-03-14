1a5f2bd1a2c8b28c04298d8dd20e43334241076f
Nirmal Vatsyayan <nirmal.vatsyayan1990@gmail.com>
2017-03-14 16:20:16 +0530

    traditional stack queue

---
+++ using_python/stack_queue/queue.py
@@ -0,0 +1,26 @@
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def enqueue(self, item):
+        self.items.insert(0,item)
+
+    def dequeue(self):
+        return self.items.pop()
+
+    def size(self):
+        return len(self.items)
+
+q = Queue()
+print("*"*50)
+print(q.isEmpty())
+print("*"*50)
+q.enqueue(10)
+q.enqueue(20)
+print(q.size())
+print("*"*50)
+print(q.dequeue())
+print(q.size())

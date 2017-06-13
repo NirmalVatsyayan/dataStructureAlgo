b3f6c1065588ae6d8203b4ebb6011d188bce0f24
Nirmal Vatsyayan <nirmal.vatsyayan1990@gmail.com>
2017-06-13 15:19:27 +0530

    tree revision

---
+++ using_python/tree/binary_search_tree/06_bst_search.py
@@ -0,0 +1,50 @@
+ 
+# A utility class that represents an individual node in a BST
+class Node:
+    def __init__(self,key):
+        self.left = None
+        self.right = None
+        self.val = key
+
+
+# A utility function to search a given key in BST
+def search(root,key):
+     
+    # Base Cases: root is null or key is present at root
+    if root is None or root.val == key:
+        return root
+ 
+    # Key is greater than root's key
+    if root.val < key:
+        return search(root.right,key)
+   
+    # Key is smaller than root's key
+    return search(root.left,key)
+ 
+
+# A utility function to insert a new node with the given key
+def insert(root,node):
+    if root is None:
+        root = node
+    else:
+        if root.val < node.val:
+            if root.right is None:
+                root.right = node
+            else:
+                insert(root.right, node)
+        else:
+            if root.left is None:
+                root.left = node
+            else:
+                insert(root.left, node)
+
+r = Node(50)
+insert(r,Node(30))
+insert(r,Node(20))
+insert(r,Node(40))
+insert(r,Node(70))
+insert(r,Node(60))
+insert(r,Node(80))
+
+x = search(r, 30)
+print(x.val)
